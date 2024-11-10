0ef555943c4ff0c64ca32e886c144072cff3fedf
saeedabdollahi1994 <saeed.abdollahi.1994@gmail.com>
2024-11-10 19:03:49 +0330

    commit2

--- object_detection.py
+++ object_detection.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
-import cv2 as cv
+
 
 class MyClass:
     def __init__(self):
@@ -7,12 +7,11 @@ class MyClass:
 
     def object_detection(self,image):
         model = YOLO("best.pt")
-       #image = cv.imread(image)
         results = model(image)
         names = model.names
         for r in results:
             for c in r.boxes.cls:
-                self.output.append(c)
-                #print(int(c),names[int(c)])
+                self.output.append(names[int(c)])
+                
 
         return self.output,results[0].plot()
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,7 @@
 import streamlit as st
-import os
 from pathlib import Path
-import torch
-from typing import Union
-import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
-from ultralytics import YOLO
 from object_detection import MyClass
 
 st.title("🎈 My new app")
@@ -15,19 +9,13 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
-image = Image.open(uploaded_file)
-img_array = np.array(image)
+
 yolo_detector = MyClass()
-text1 = type(img_array)
-st.image(image, caption= text1)
-'''
-plt.imshow(uploaded_file.plot())  # Plot the bounding boxes on the image
-plt.axis("off")
-plt.show()
+
+#st.image(image, caption= text1)
 
 if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    img_array = np.array(image)
     output,detected_image = yolo_detector.object_detection(img_array)
-    img = cv.imread(detected_image)
-    st.write(output)
-    st.image(img, caption=output)
-'''
+    st.image(detected_image, caption=output)
